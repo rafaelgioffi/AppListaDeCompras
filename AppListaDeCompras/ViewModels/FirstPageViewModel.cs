ff485af7796ffd6a993c62dbb8ba11ddaf28c2cc
Rafael Gioffi <rafaelgioffi@gmail.com>
2024-08-09 23:58:50 -0300

    Add project files.

---
+++ AppListaDeCompras/ViewModels/FirstPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListaDeCompras.ViewModels
+{
+    partial class FirstPageViewModel : ObservableObject
+    {
+        [RelayCommand]
+        private void NavigateToAppShell()
+        {
+            App.Current.MainPage = new AppShell();
+        }
+    }
+}
