465ed4f0f520156a85cc8e486f45423b6a90187e
Rafael Gioffi <rafaelgioffi@gmail.com>
2024-08-11 00:59:26 -0300

    Lesson 18

---
+++ AppListaDeCompras/Models/ListToBuy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListaDeCompras.Models
+{
+    public class ListToBuy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<Product> Products { get; set; } = new List<Product>();
+        public List<User> Users { get; set; }
+        public DateTimeOffset CreateAt { get; set; }
+    }
+}
---
+++ AppListaDeCompras/ViewModels/ListToBuyViewModel.cs
@@ -0,0 +1,55 @@
+﻿using AppListaDeCompras.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace AppListaDeCompras.ViewModels
+{
+    public partial class ListToBuyViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<ListToBuy> _listToBuy;
+        public ListToBuyViewModel()
+        {
+            ListToBuy = new ObservableCollection<ListToBuy>() {
+        new ListToBuy() {
+            Id = 1,
+        Name = "Minha Lista 1",
+        Users = new List<User>()
+        {
+            new User {
+                Name = "Rafael",
+                Email="rafaegioffi@gmail.com"
+            },
+            new User {
+                Name = "Bruna",
+                Email="brunarpereira@hotmail.com"
+            }
+            },
+            Products = new List<Product>()
+            {
+                new Product { },
+                new Product { },
+                new Product { },
+        },
+        CreateAt = DateTime.Now,
+        },
+
+        new ListToBuy()
+        {
+            Id = 2,
+            Name = "Minha Lista 2",
+        Users = new List<User>()
+        {
+            new User {
+                Name = "Rafael",
+                Email="rafaegioffi@gmail.com"
+            },
+        },
+        CreateAt = DateTime.Now.AddDays(-7),
+
+        }
+
+    };
+        }
+    }
+}
