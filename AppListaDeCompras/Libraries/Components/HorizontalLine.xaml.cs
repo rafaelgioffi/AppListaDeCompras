903e72ee7ee57e33357b5c3d7d4c1e27b1cb20a9
Rafael Gioffi <rafaelgioffi@gmail.com>
2024-08-11 23:33:45 -0300

    Lesson 19

---
+++ AppListaDeCompras/Libraries/Components/HorizontalLine.xaml.cs
@@ -0,0 +1,21 @@
+using System.Runtime.CompilerServices;
+
+namespace AppListaDeCompras.Libraries.Components;
+
+public partial class HorizontalLine : ContentView
+{
+    private Color _stroke;
+	public HorizontalLine()
+	{
+		InitializeComponent();
+	}
+
+    protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        base.OnPropertyChanged(propertyName);
+
+        if (Window?.Width != null & Line != null) {
+            Line.X2 = Window.Width;
+        }
+    }
+}
