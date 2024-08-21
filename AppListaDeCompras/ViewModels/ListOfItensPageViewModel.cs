cde6259f82c60d04d02f63ba71e4a31a06b5a600
Rafael Gioffi <rafaelgioffi@gmail.com>
2024-08-20 23:48:18 -0300

    Lesson 28

---
+++ AppListaDeCompras/ViewModels/ListOfItensPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using AppListaDeCompras.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AppListaDeCompras.ViewModels
+{
+    [QueryProperty(nameof(ListToBuy), "ListToBuy")]
+    public partial class ListOfItensPageViewModel : ObservableObject
+    {
+        private ListToBuy? _listToBuy;
+
+        public ListToBuy? ListToBuy
+        {
+            get => _listToBuy;
+            set => SetProperty(ref _listToBuy, value);
+        }
+    }
+}
