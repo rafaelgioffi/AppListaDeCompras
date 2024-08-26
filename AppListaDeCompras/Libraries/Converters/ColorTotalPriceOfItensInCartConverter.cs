36b604c0c5b20c5635947cc1f12ccc21c74ad812
Rafael Gioffi <rafaelgioffi@gmail.com>
2024-08-25 23:13:28 -0300

    Lesson 35

---
+++ AppListaDeCompras/Libraries/Converters/ColorTotalPriceOfItensInCartConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Globalization;
+
+namespace AppListaDeCompras.Libraries.Converters
+{
+    public class ColorTotalPriceOfItensInCartConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            bool hasCaught = (bool)value;
+
+            if (hasCaught)
+            {
+                if (App.Current.PlatformAppTheme == AppTheme.Light)
+                {
+                    return Colors.Black;
+                }
+                else if (App.Current.PlatformAppTheme == AppTheme.Dark)
+                {
+                    return Colors.White;
+                }
+            }            
+                    return Colors.DarkGray;
+            //return (hasCaught) ? Colors.Black : Colors.LightGray;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
